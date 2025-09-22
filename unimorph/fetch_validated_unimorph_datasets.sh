5f86d3051bded7d5a52cd94dee9c2a7c091a5b6a
Viktor Sharha <vsharha07@gmail.com>
2025-09-22 15:11:51 +0100

    updated fetch logic

--- unimorph/fetch_validated_unimorph_datasets.sh
+++ unimorph/fetch_validated_unimorph_datasets.sh
@@ -3,8 +3,17 @@ set -euxo pipefail
 
 # python iso_639/check_iso_code.py --codes unimorph_repos.txt > validated_codes.txt
 
-while read -r code; done
-    curl -L -o "${code}/${code.txt}" "https://github.com/unimorph/${code}/blob/master/${code}"
+while read -r code; do
+    output_dir="unimorph/data/${code}"
+    output_file="${code}.tab"
+
+    if [[ -f "$output_dir/$output_file" ]]; then
+        echo "Skipping ${code} - file already exists: ${output_file}"
+        continue
+    fi
+
+    mkdir -p ${output_dir}
+    curl -L -o "${output_dir}/${output_file}" "https://raw.githubusercontent.com/unimorph/${code}/refs/heads/master/${code}"
     echo "Downloaded ${code} dataset"
     sleep 5
 done < validated_codes.txt
